41cc43754560a2ee2ec5e3a1965f45c47939bf31
trchudley <24896931+trchudley@users.noreply.github.com>
2024-04-22 17:51:06 +0100

    fix __init__.py versioning error

--- src/strain_tools/__init__.py
+++ src/strain_tools/__init__.py
@@ -12,24 +12,23 @@ from ._strain_rates import (
 
 from . import _cli
 
-__version__ = version("pdemtools")
+__version__ = "0.2.0"
 
 
-# # for testing only - reload modules ---------
-from importlib import reload
+# # # for testing only - reload modules ---------
+# from importlib import reload
 
-from . import _strain_rates
+# from . import _strain_rates
 
-reload(_strain_rates)
+# reload(_strain_rates)
 
-from ._strain_rates import (
-    log_strain_rates,
-    principal_strain_rate_directions,
-    principal_strain_rate_magnitudes,
-    flow_direction,
-    rotated_strain_rates,
-    effective_strain_rate,
-    strain_rate_uncertainty,
-)
-
-# # --------------------------
+# from ._strain_rates import (
+#     log_strain_rates,
+#     principal_strain_rate_directions,
+#     principal_strain_rate_magnitudes,
+#     flow_direction,
+#     rotated_strain_rates,
+#     effective_strain_rate,
+#     strain_rate_uncertainty,
+# )
+# # # --------------------------
